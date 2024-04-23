49e41bec61c397752a2fa4533185b381a95c84c9
hbns <vdohans@gmail.com>
2024-04-23 15:44:46 +0200

    reconsider deployment

--- lib/hvm.ex
+++ lib/hvm.ex
@@ -2,19 +2,30 @@ defmodule Hvm do
   # module attributes, can only be static.
   # @signal_table %{time: fn -> Time.utc_now() end}
   @signal_table %{time: 26}
+  @native_table [:plus, :minus, :divide, :multiply]
 
   # Start the VM for received byte code
   def run_VM(reactor_byte_code) do
     # reactors_catalog: key = reactor_name and value = {nos_src, nos_snk, dti, rti}.
     {:ok, reactors_catalog} = catalog_reactors(reactor_byte_code)
 
+    #IO.inspect(reactors_catalog, label: 'Reactor_catalog: ')
+    dti_allocations = prepare_deployments(reactors_catalog)
+    filtered_dti_allocations = drop_native_keys(dti_allocations)
+    IO.inspect(dti_allocations, label: ~c"Total dti_allocations: ")
+    IO.inspect(filtered_dti_allocations, label: ~c"filtered dti_allocations: ")
+
+
+
     # read all reactors dti, prepare dtm blocks, deploy and store key:reactor_name, value: deployment_pid.
     deployment_pids =
-      Enum.reduce(reactors_catalog, %{}, fn {reactor_name, reactor}, deployment_pids ->
+     Enum.reduce(reactors_catalog, %{}, fn {reactor_name, reactor}, deployment_pids ->
         # pattern match reactor
-        {_nos_src, _nos_snk, dti, rti} = reactor
-        # transform deployment-time-instrcutions (dti) into deployment-time-memroy (dtm)
+       {_td, _nos_src, _nos_snk, dti, rti} = reactor
+       # transform deployment-time-instrcutions (dti) into deployment-time-memroy (dtm)
         dtm_blocks = make_dtm_blocks(dti, reactors_catalog)
+        #IO.inspect(dtm_blocks, label: ~c"dtmBlocks: ")
+
         # deploy the reactor (start genserver for this reactor) and receive pid
         deployment_pid = deploy_reaktor(dtm_blocks, List.duplicate(nil, length(rti)), rti)
         # update deployment_pids key:reactor_name value:deployment_pid
@@ -23,17 +34,50 @@ defmodule Hvm do
         updated_deployment_pids
       end)
 
+     #IO.inspect(reactors_catalog, label: ~c"Rcatalog")
+
+    # per deployment deployments maken,
+
     # Loads each deployment pid in all deployments, each deployment knows the pid of all other deployments.
-    Enum.each(deployment_pids, fn {_reactor_name, deployment_pid} ->
-      Memory.load_pids(deployment_pid, deployment_pids)
-    end)
+#    Enum.each(deployment_pids, fn {_reactor_name, deployment_pid} ->
+#      Memory.load_pids(deployment_pid, deployment_pids)
+#    end)
 
     # Start looping the deployment
     # second argument is times to itterate
-    loop_deployment(deployment_pids, 10)
+    #    loop_deployment(deployment_pids, 10)
 
     IO.puts("vm stopped")
   end
+
+  def prepare_deployments(reactors_catalog) do
+    total_occurrences =
+      Enum.reduce(reactors_catalog, %{}, fn {_, reactor}, acc ->
+        {_td, _nos_src, _nos_snk, dti, _rti} = reactor
+        count = count_occurrences(dti)
+       # IO.inspect(count, label: ~c"count Occurrences: ")
+        Map.merge(acc, count, fn _key, val1, val2 -> val1 + val2 end)
+      end)
+
+    #IO.inspect(total_occurrences, label: ~c"Total Occurrences: ")
+  end
+
+  defp count_occurrences(list) do
+    Enum.reduce(list, %{}, fn ["I-ALLOCMONO", name], acc ->
+      Map.update(acc, name, 1, &(&1 + 1))
+    end)
+  end
+  def drop_native_keys(map) do
+    #@native_table = [:plus, :minus, :divide, :multiply] # Assuming this module attribute is defined
+
+    Enum.reduce(@native_table, map, fn key, acc ->
+      Map.delete(acc, key)
+    end)
+  end
+
+
+
+
 
   # basecase, to loop n times..
   def loop_deployment(_deployment_pids, 0) do
@@ -67,104 +111,14 @@ defmodule Hvm do
     ]
   end
 
-  # Help to run start
-  def run_start do
-    # test reactor:
-    pto = [
-      [
-        :main,
-        1,
-        1,
-        [
-          ["I-ALLOCMONO", :plus],
-          ["I-ALLOCMONO", :plus]
-        ],
-        [
-          ["I-LOOKUP", :time],
-          ["I-SUPPLY", ["%RREF", 1], ["%DREF", 1], 1],
-          ["I-SUPPLY", ["%SRC", 1], ["%DREF", 1], 2],
-          ["I-REACT", ["%DREF", 1]],
-          ["I-CONSUME", ["%DREF", 1], 1],
-          ["I-SUPPLY", ["%RREF", 5], ["%DREF", 2], 1],
-          ["I-SUPPLY", 1, ["%DREF", 2], 2],
-          ["I-REACT", ["%DREF", 2]],
-          ["I-CONSUME", ["%DREF", 2], 1],
-          ["I-SINK", ["%RREF", 9], 1]
-        ]
-      ]
-    ]
-
-    mt = [
-      [
-        :plus_time_one,
-        1,
-        1,
-        [["I-ALLOCMONO", :plus], ["I-ALLOCMONO", :plus]],
-        [
-          ["I-LOOKUP", :time],
-          ["I-SUPPLY", ["%RREF", 1], ["%DREF", 1], 1],
-          ["I-SUPPLY", ["%SRC", 1], ["%DREF", 1], 2],
-          ["I-REACT", ["%DREF", 1]],
-          ["I-CONSUME", ["%DREF", 1], 1],
-          ["I-SUPPLY", ["%RREF", 5], ["%DREF", 2], 1],
-          ["I-SUPPLY", 1, ["%DREF", 2], 2],
-          ["I-REACT", ["%DREF", 2]],
-          ["I-CONSUME", ["%DREF", 2], 1],
-          ["I-SINK", ["%RREF", 9], 1]
-        ]
-      ],
-      [
-        :plus_time_five,
-        1,
-        1,
-        [["I-ALLOCMONO", :plus], ["I-ALLOCMONO", :plus]],
-        [
-          ["I-LOOKUP", :time],
-          ["I-SUPPLY", ["%RREF", 1], ["%DREF", 1], 1],
-          ["I-SUPPLY", ["%SRC", 1], ["%DREF", 1], 2],
-          ["I-REACT", ["%DREF", 1]],
-          ["I-CONSUME", ["%DREF", 1], 1],
-          ["I-SUPPLY", ["%RREF", 5], ["%DREF", 2], 1],
-          ["I-SUPPLY", 5, ["%DREF", 2], 2],
-          ["I-REACT", ["%DREF", 2]],
-          ["I-CONSUME", ["%DREF", 2], 1],
-          ["I-SINK", ["%RREF", 9], 1]
-        ]
-      ],
-      [
-        :main,
-        2,
-        1,
-        [
-          ["I-ALLOCMONO", :plus_time_one],
-          ["I-ALLOCMONO", :plus_time_five],
-          ["I-ALLOCMONO", :minus]
-        ],
-        [
-          ["I-SUPPLY", ["%SRC", 1], ["%DREF", 1], 1],
-          ["I-REACT", ["%DREF", 1]],
-          ["I-SUPPLY", ["%SRC", 2], ["%DREF", 2], 1],
-          ["I-REACT", ["%DREF", 2]],
-          ["I-CONSUME", ["%DREF", 2], 1],
-          ["I-SUPPLY", ["%RREF", 5], ["%DREF", 3], 1],
-          ["I-CONSUME", ["%DREF", 1], 1],
-          ["I-SUPPLY", ["%RREF", 7], ["%DREF", 3], 2],
-          ["I-REACT", ["%DREF", 3]],
-          ["I-CONSUME", ["%DREF", 3], 1],
-          ["I-SINK", ["%RREF", 10], 1]
-        ]
-      ]
-    ]
-
-    run_VM(mt)
-  end
-
   # Make key value map, key = reactor_name and value = reactor -> {nos_src, nos_snk, dti, rti}.
   defp catalog_reactors([], reactors_catalog \\ %{}), do: {:ok, reactors_catalog}
 
   defp catalog_reactors([[name, nos_src, nos_snk, dti, rti] | tail], reactors_catalog) do
+    # to keep track how many times a reactor is deployed
+    td = 0
     # add reactor to the map.
-    updated_reactors_catalog = Map.put(reactors_catalog, name, {nos_src, nos_snk, dti, rti})
+    updated_reactors_catalog = Map.put(reactors_catalog, name, {td, nos_src, nos_snk, dti, rti})
 
     # recurse and accumulate...
     catalog_reactors(tail, updated_reactors_catalog)
@@ -174,7 +128,7 @@ defmodule Hvm do
   defp make_dtm_blocks([], _reactors, acc \\ []), do: Enum.reverse(acc)
 
   defp make_dtm_blocks([["I-ALLOCMONO", name] | rest], reactors, acc) do
-    # load rti into dtm block, if rti not found, state :native to show this is a native reactor.
+    # define reactor type
     type =
       if Map.has_key?(reactors, name) do
         :user_defined
@@ -182,10 +136,34 @@ defmodule Hvm do
         :native
       end
 
+    new_name =
+      if type == :user_defined do
+        {td, _nos_src, _nos_snk, _dti, _rti} = Map.get(reactors, name)
+        if td > 0 do
+          :newname
+        else
+          name
+        end
+      else
+        name
+      end
+
+
+    # update the times of deployment td of the user_defined reactor
+    updated_reactors =
+      if type == :user_defined do
+        {td, _nos_src, _nos_snk, _dti, _rti} = Map.get(reactors, name)
+        updated_td = td + 1
+        Map.put(reactors, name, {updated_td, _nos_src, _nos_snk, _dti, _rti})
+      else
+        reactors
+      end
+
     # make the dtm block
-    block = {name, [nil], [], type, [nil]}
+    block = {new_name, [nil], [], type, [nil]}
     # recurse for each dtm block to be allocated
-    make_dtm_blocks(rest, reactors, [block | acc])
+    #IO.inspect(updated_reactors, label: 'udr: ')
+    make_dtm_blocks(rest, updated_reactors, [block | acc])
   end
 
   # Deploy the reaktor
@@ -193,11 +171,11 @@ defmodule Hvm do
     case Memory.start_link(dtm, rtm, rti, %{}, [0], [0]) do
       {:ok, pid} ->
         # Use the pid here
-        IO.puts("GenServer started with PID: #{inspect(pid)}")
+        #IO.puts("GenServer started with PID: #{inspect(pid)}")
         pid
 
       {:error, reason} ->
-        IO.puts("Failed to start GenServer: #{reason}")
+        #IO.puts("Failed to start GenServer: #{reason}")
     end
   end
 
@@ -212,7 +190,7 @@ defmodule Hvm do
         Enum.each(Enum.with_index(rti), fn
           {instruction, rti_index} ->
             handle_instruction(instruction, rti_index, pid)
-          # Memory.show_state(pid)
+            # Memory.show_state(pid)
         end)
     end
   end
@@ -324,6 +302,157 @@ defmodule Hvm do
 
   # push to supercollider, takes the values and send the osc message to sc
   defp push_to_sc(args) do
+  end
 
+  # Help to run start
+  def run_start do
+    # test reactor:
+    pto = [
+      [
+        :main,
+        1,
+        1,
+        [
+          ["I-ALLOCMONO", :plus],
+          ["I-ALLOCMONO", :plus]
+        ],
+        [
+          ["I-LOOKUP", :time],
+          ["I-SUPPLY", ["%RREF", 1], ["%DREF", 1], 1],
+          ["I-SUPPLY", ["%SRC", 1], ["%DREF", 1], 2],
+          ["I-REACT", ["%DREF", 1]],
+          ["I-CONSUME", ["%DREF", 1], 1],
+          ["I-SUPPLY", ["%RREF", 5], ["%DREF", 2], 1],
+          ["I-SUPPLY", 1, ["%DREF", 2], 2],
+          ["I-REACT", ["%DREF", 2]],
+          ["I-CONSUME", ["%DREF", 2], 1],
+          ["I-SINK", ["%RREF", 9], 1]
+        ]
+      ]
+    ]
+
+    mt = [
+      [
+        :plus_time_one,
+        1,
+        1,
+        [["I-ALLOCMONO", :plus], ["I-ALLOCMONO", :plus]],
+        [
+          ["I-LOOKUP", :time],
+          ["I-SUPPLY", ["%RREF", 1], ["%DREF", 1], 1],
+          ["I-SUPPLY", ["%SRC", 1], ["%DREF", 1], 2],
+          ["I-REACT", ["%DREF", 1]],
+          ["I-CONSUME", ["%DREF", 1], 1],
+          ["I-SUPPLY", ["%RREF", 5], ["%DREF", 2], 1],
+          ["I-SUPPLY", 1, ["%DREF", 2], 2],
+          ["I-REACT", ["%DREF", 2]],
+          ["I-CONSUME", ["%DREF", 2], 1],
+          ["I-SINK", ["%RREF", 9], 1]
+        ]
+      ],
+      [
+        :plus_time_five,
+        1,
+        1,
+        [["I-ALLOCMONO", :plus], ["I-ALLOCMONO", :plus]],
+        [
+          ["I-LOOKUP", :time],
+          ["I-SUPPLY", ["%RREF", 1], ["%DREF", 1], 1],
+          ["I-SUPPLY", ["%SRC", 1], ["%DREF", 1], 2],
+          ["I-REACT", ["%DREF", 1]],
+          ["I-CONSUME", ["%DREF", 1], 1],
+          ["I-SUPPLY", ["%RREF", 5], ["%DREF", 2], 1],
+          ["I-SUPPLY", 5, ["%DREF", 2], 2],
+          ["I-REACT", ["%DREF", 2]],
+          ["I-CONSUME", ["%DREF", 2], 1],
+          ["I-SINK", ["%RREF", 9], 1]
+        ]
+      ],
+      [
+        :main,
+        2,
+        1,
+        [
+          ["I-ALLOCMONO", :plus_time_one],
+          ["I-ALLOCMONO", :plus_time_five],
+          ["I-ALLOCMONO", :minus]
+        ],
+        [
+          ["I-SUPPLY", ["%SRC", 1], ["%DREF", 1], 1],
+          ["I-REACT", ["%DREF", 1]],
+          ["I-SUPPLY", ["%SRC", 2], ["%DREF", 2], 1],
+          ["I-REACT", ["%DREF", 2]],
+          ["I-CONSUME", ["%DREF", 2], 1],
+          ["I-SUPPLY", ["%RREF", 5], ["%DREF", 3], 1],
+          ["I-CONSUME", ["%DREF", 1], 1],
+          ["I-SUPPLY", ["%RREF", 7], ["%DREF", 3], 2],
+          ["I-REACT", ["%DREF", 3]],
+          ["I-CONSUME", ["%DREF", 3], 1],
+          ["I-SINK", ["%RREF", 10], 1]
+        ]
+      ]
+    ]
+
+    mt2 = [
+      [
+        :plus_time_one,
+        1,
+        1,
+        [["I-ALLOCMONO", :plus], ["I-ALLOCMONO", :plus]],
+        [
+          ["I-LOOKUP", :time],
+          ["I-SUPPLY", ["%RREF", 1], ["%DREF", 1], 1],
+          ["I-SUPPLY", ["%SRC", 1], ["%DREF", 1], 2],
+          ["I-REACT", ["%DREF", 1]],
+          ["I-CONSUME", ["%DREF", 1], 1],
+          ["I-SUPPLY", ["%RREF", 5], ["%DREF", 2], 1],
+          ["I-SUPPLY", 1, ["%DREF", 2], 2],
+          ["I-REACT", ["%DREF", 2]],
+          ["I-CONSUME", ["%DREF", 2], 1],
+          ["I-SINK", ["%RREF", 9], 1]
+        ]
+      ],
+      [
+        :plus_time_five,
+        1,
+        1,
+        [["I-ALLOCMONO", :plus_time_one], ["I-ALLOCMONO", :plus]],
+        [
+          ["I-SUPPLY", ["%SRC", 1], ["%DREF", 1], 1],
+          ["I-REACT", ["%DREF", 1]],
+          ["I-CONSUME", ["%DREF", 1], 1],
+          ["I-SUPPLY", ["%RREF", 3], ["%DREF", 2], 1],
+          ["I-SUPPLY", 5, ["%DREF", 2], 2],
+          ["I-REACT", ["%DREF", 2]],
+          ["I-CONSUME", ["%DREF", 2], 1],
+          ["I-SINK", ["%RREF", 7], 1]
+        ]
+      ],
+      [
+        :main,
+        2,
+        1,
+        [
+          ["I-ALLOCMONO", :plus_time_one],
+          ["I-ALLOCMONO", :plus_time_five],
+          ["I-ALLOCMONO", :minus]
+        ],
+        [
+          ["I-SUPPLY", ["%SRC", 1], ["%DREF", 1], 1],
+          ["I-REACT", ["%DREF", 1]],
+          ["I-SUPPLY", ["%SRC", 2], ["%DREF", 2], 1],
+          ["I-REACT", ["%DREF", 2]],
+          ["I-CONSUME", ["%DREF", 2], 1],
+          ["I-SUPPLY", ["%RREF", 5], ["%DREF", 3], 1],
+          ["I-CONSUME", ["%DREF", 1], 1],
+          ["I-SUPPLY", ["%RREF", 7], ["%DREF", 3], 2],
+          ["I-REACT", ["%DREF", 3]],
+          ["I-CONSUME", ["%DREF", 3], 1],
+          ["I-SINK", ["%RREF", 10], 1]
+        ]
+      ]
+    ]
+
+    run_VM(mt2)
   end
 end
--- lib/test_collider.ex
+++ lib/test_collider.ex
@@ -7,21 +7,25 @@ defmodule Test_collider do
   end
 
   def play() do
-    SuperCollider.command(:s_new, ["note_player", 100, 1, 0, ["freq", 128, "amp", 0.1]])
+    SuperCollider.command(:s_new, ["note_player", 100, 1, 0, ["freq", 328, "amp", 0.1]])
     Process.sleep(500)
     SuperCollider.command(:n_free, 100)
     Process.sleep(500)
-    SuperCollider.command(:s_new, ["sine", 100, 1, 0, ["freq", 256, "amp", 0.1]])
+    SuperCollider.command(:s_new, ["sine", 100, 1, 0, ["freq", 656, "amp", 0.1]])
     Process.sleep(500)
     SuperCollider.command(:n_free, 100)
     Process.sleep(500)
-    SuperCollider.command(:s_new, ["sine", 100, 1, 0, ["freq", 512, "amp", 0.1]])
+    SuperCollider.command(:s_new, ["sine", 100, 1, 0, ["freq", 912, "amp", 0.1]])
     Process.sleep(500)
     SuperCollider.command(:n_free, 100)
     Process.sleep(500)
 
   end
+
 end
+
+# jampa actuator functie
+#
 
 # SuperCollider.command(:s_new, ["note_player", 100, 1, 0, ["freq", 426, "amp", 0.1]])
 #SuperCollider.command(:n_set, [100, ["gate", 1]])
